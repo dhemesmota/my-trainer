944d186c10021694afbb7e7117808500ca4fe4e4
Dhemes Mota <dhemes.mota@gmail.com>
2025-08-23 19:25:26 -0300

    fix: configura variáveis de ambiente do Supabase no next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
   /* config options here */
 };
 
